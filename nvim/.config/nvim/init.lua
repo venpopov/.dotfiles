0692cb8e88fd3012847f2df5f0ff2f9663a53938
Ven Popov <vencislav.popov@gmail.com>
2025-02-14 12:29:04 +0100

    switch to relative line numbers

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,6 +2,6 @@ vim.opt.showmatch = true
 vim.opt.tabstop = 2
 vim.opt.expandtab = true
 vim.opt.autoindent = true
-vim.opt.number = true
+vim.wo.relativenumber = true
 vim.opt.cc = "120"
 vim.opt.syntax = on
