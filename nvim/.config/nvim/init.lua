393678a2c94fa44d70374f36391c9eb3a7812a41
Ven Popov <vencislav.popov@gmail.com>
2025-02-14 12:36:03 +0100

    make 2 spaces the default tab width

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,8 @@
 vim.opt.showmatch = true
+vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.expandtab = true
+vim.bo.softtabstop = 2
 vim.opt.autoindent = true
 vim.wo.relativenumber = true
 vim.opt.cc = "120"
